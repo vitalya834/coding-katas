ae4d996647d9f3bdb35933fabf38333f799c71a8
Joe Ray Gregory @ Coding Bootcamps Europe <122170539+joegregory-cbe@users.noreply.github.com>
2023-10-05 23:37:29 +0200

    Update zip-strings.js

--- zip-strings/zip-strings.js
+++ zip-strings/zip-strings.js
@@ -1,18 +1,3 @@
 function zipStrings(strA, strB) {
-  let result = "";
-  const maxLength = Math.max(strA.length, strB.length);
-
-  for (let i = 0; i < maxLength; i++) {
-    /*     if (i < strA.length) {
-      result += strA[i];
-    }
-
-    if (i < strB.length) {
-      result += strB[i];
-    } */
-
-    result += (strA[i] || "") + (strB[i] || "");
-  }
-
-  return result;
+  // Your Code here
 }
