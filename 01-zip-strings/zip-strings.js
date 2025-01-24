abc9336fa34d9110affbf3ce0dea0d47151deb1c
vitalya834 <44960212+vitalya834@users.noreply.github.com>
2025-01-24 10:11:11 +0100

    init

--- 01-zip-strings/zip-strings.js
+++ 01-zip-strings/zip-strings.js
@@ -1,3 +1,18 @@
 function zipStrings(strA, strB) {
+let result = "";
+let maxLength = Math.max(strA.length, strB.length);   // suchen max length
+  for (let i=0; i <maxLength; i++) {
+    if (i<strA.length) {  // check  gibt es noch ein Buchstabe in strA
+      result += strA[i];   // addiere Buchstabe von strA
+    }
+    if (i<strB.length) {  // check  gibt es noch ein Buchstabe in strB
+      result += strB[i];   // addiere Buchstabe von strB
+    }
+
+
+  }
+
+  return result;
+
   // Your Code here
 }
--- 02-extract-class-name/extract-class-name.js
+++ 02-extract-class-name/extract-class-name.js
@@ -1,3 +1,23 @@
 function extractClassName(sessionTitle) {
-  // Add you solution here
-}
+  
+
+
+   const months = {
+    "Januar": "01",
+    "Februar": "02",
+    "März": "03",
+    "Maerz": "03",
+    "April": "04",
+    "Mai": "05",
+    "Juni": "06",
+    "Juli": "07",
+    "August": "08",
+    "September": "09",
+    "Oktober": "10",
+    "November": "11",
+    "Dezember": "12"
+};
+  const parts= input.split(" ");
+  if (parts.length !== 3) {
+    return null;
+  }
