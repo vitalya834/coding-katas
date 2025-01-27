4d81f24ca5850cd0005c38d56454232aa14e2872
Vitaly Belykh <vitalyablyh@gmail.com>
2025-01-27 10:24:04 +0100

    03

--- 03-filter-api-data/filter-api-data.js
+++ 03-filter-api-data/filter-api-data.js
@@ -1,3 +1,28 @@
 function filterApiData(apiData, mandatoryKeys) {
-  // Add you solution here
+    // massiv       
+  let result = [];
+
+  // jaden obj in  apiData
+  for (let i = 0; i < apiData.length; i++) {
+      const obj = apiData[i]; //   this obj 
+      let hasAllKeys = true; //   thisi  is key       gibt es 
+
+      //  jeden key   aus  mandatoryKeys
+      for (let j = 0; j < mandatoryKeys.length; j++) {
+          const key = mandatoryKeys[j]; // this is key
+
+          //   if key is keine  
+          if (!(key in obj)) {
+              hasAllKeys = false; //  select  flage  false
+              break;  
+          }
+      }
+
+      // add  key  
+      if (hasAllKeys) {
+          result.push(obj);
+      }
+  }
+
+  return result; // !!
 }
